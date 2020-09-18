55897dd9e78c1fa2f9073d491d4fc1ed9544b149
Mikel Sanabria <mikel.sanabria@gmail.com>
2020-09-18 20:01:12 +0200

    update method C1 #1

--- src/main/java/es/upm/miw/iwvg_devops/rest/C1.java
+++ src/main/java/es/upm/miw/iwvg_devops/rest/C1.java
@@ -3,5 +3,6 @@ package es.upm.miw.iwvg_devops.rest;
 public class C1 {
     public void testMethod(){
         System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo2");
     }
 }
