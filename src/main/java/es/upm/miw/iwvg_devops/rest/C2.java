bcb7421ff65fefa2eb7b920b351fe1d3b54cf58e
Mikel Sanabria <mikel.sanabria@gmail.com>
2020-09-18 20:28:08 +0200

    update method C2 #1

--- src/main/java/es/upm/miw/iwvg_devops/rest/C2.java
+++ src/main/java/es/upm/miw/iwvg_devops/rest/C2.java
@@ -1,4 +1,8 @@
 package es.upm.miw.iwvg_devops.rest;
 
 public class C2 {
+    public void testMethodC2(){
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo2");
+    }
 }
